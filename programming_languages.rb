532470298a94dcc0fb379425a1f88e65e7380f54
Nicole Dow <nikki.dow71@gmail.com>
2019-06-05 17:57:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -77,16 +77,17 @@ def reformat_languages(languages)
   
   #iterate through original languages hash down to language names
   languages.each do |lang_style, lang_group|
-    
-    #for each 
     lang_group.each do |lang, lang_info|
+       #for each, check if the lang has already been added to new_hash
+       #if it has, add this style to the style array
       if new_hash.has_key?(lang)
-        
+        new_hash[lang][:style] << lang_style
+      #if not, create new hash for info within new_hash:
       else
         new_hash[lang] = {}
         new_hash[lang][:type] = lang_info[:type]
         new_hash[lang][:style] = []
-        new_hash[lang][:style] << 
+        new_hash[lang][:style] << lang_style
       end
     end
   end
