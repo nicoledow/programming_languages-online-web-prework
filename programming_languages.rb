e190d4569e3a78336fb94d979ed5dedba182eab2
Nicole Dow <nikki.dow71@gmail.com>
2019-06-05 17:42:50 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -76,7 +76,7 @@ def reformat_languages(languages)
   new_hash = {}
   
   #iterate through original languages hash 
-  languages.each do |style__group, lang|
+  languages.each do |style_group, lang|
     binding.pry
   end
   
